6a51d7a853c68b7911cd0994a7a284da6d045a0a
Alexander <48152200+Sisha3342@users.noreply.github.com>
2019-03-24 18:31:17 +0300

    Add files via upload
    Added default interface

---
+++ Vigener_encrypt/Encrypt_interface.h
@@ -0,0 +1,32 @@
+﻿
+// ROFLAN_MFC.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить stdafx.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CROFLANMFCApp:
+// Сведения о реализации этого класса: ROFLAN_MFC.cpp
+//
+
+class CROFLANMFCApp : public CWinApp
+{
+public:
+	CROFLANMFCApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CROFLANMFCApp theApp;
